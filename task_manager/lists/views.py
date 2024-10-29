faaee9ff502561a409c984c7b5bc4050a078d60f
Vinda <vinda@smartshore.nl>
2024-10-29 14:33:03 +0530

    Added try catch block

--- task_manager/lists/views.py
+++ task_manager/lists/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from .forms import ListForm, ItemForm
 from .models import List, Item
 
+
 # Create your views here.
 @login_required
 def list_view(request):
@@ -15,13 +16,15 @@ def list_view(request):
     except Exception as e:
         return JsonResponse({"error": "Unable to retrieve lists at this time. Please try again later."}, status=500)
 
-#Api to get lists
+
+# Api to get lists
 def get_list(request):
     try:
         lists = List.objects.filter(user=request.user)
         return JsonResponse(list(lists), safe=False)
     except Exception as e:
         return JsonResponse({"error": "Unable to retrieve lists at this time. Please try again later."}, status=500)
+
 
 # Create List
 @login_required
@@ -31,29 +34,41 @@ def add_list(request):
         name = data.get('name')
         if not name:
             return JsonResponse({"error": "List name is required."}, status=400)
-        new_list = List.objects.create(name=name, user=request.user)
-        return JsonResponse({"message": "List created successfully.", "list_id": new_list.id})
+        try:
+            new_list = List.objects.create(name=name, user=request.user)
+            return JsonResponse({"message": "List created successfully.", "list_id": new_list.id})
+        except Exception:
+            return JsonResponse({'error': 'Unable to add lists at this time. Please try again later.'}, status=404)
     return JsonResponse({'error': 'Invalid request'}, status=400)
+
 
 @login_required
 def add_item(request):
     data = request.POST
     item_id = data.get('item_id')
-    if(item_id):
+    if (item_id):
         item = get_object_or_404(Item, id=item_id)
         form = ItemForm(request.POST, instance=item)
         if form.is_valid():
-            form.save()
+            try:
+                form.save()
+            except Exception as e:
+                return JsonResponse({'error': 'Unable to update item at this time. Please try again later.'},
+                                    status=404)
         return JsonResponse({"message": "Item Saved", "item_id": item_id})
     else:
         item_name = data.get('name')
         list_id = data.get('list_id')
         color = data.get('color')
-        list_obj = List.objects.get(id=list_id, user=request.user)
         if not item_name:
             return JsonResponse({"error": "Item name is required."}, status=400)
-        new_item = Item.objects.create(list=list_obj, name=item_name, color=color)
+        try:
+            list_obj = List.objects.get(id=list_id, user=request.user)
+            new_item = Item.objects.create(list=list_obj, name=item_name, color=color)
+        except Exception as e:
+            return JsonResponse({'error': 'Unable to add item at this time. Please try again later.'}, status=404)
         return JsonResponse({"message": "Item added to list", "item_id": new_item.id})
+
 
 # List Detail with Items
 @login_required
@@ -61,15 +76,15 @@ def list_detail(request, list_id, sort_order):
     try:
         # Fetch the list from the database
         list_data = get_object_or_404(List, id=int(list_id))
-
         item_data = Item.objects.filter(list_id=list_data, completed=False).order_by(sort_order)
         completed_items = Item.objects.filter(list_id=list_data, completed=True).order_by(sort_order)
-
         itemForm = ItemForm()
-        return render(request, 'list/list_detail.html', {'list_name': list_data, 'listItems': item_data, 'completed_items': completed_items,
-                                                         'itemForm': itemForm})
+        return render(request, 'list/list_detail.html',
+                      {'list_name': list_data, 'listItems': item_data, 'completed_items': completed_items,
+                       'itemForm': itemForm})
     except Exception:
-        return JsonResponse({'error': 'List not found'}, status=404)
+        return JsonResponse({'error': 'Unable to get list details at the moment. Please try again later.'}, status=404)
+
 
 # Mark Item as Completed
 @login_required
@@ -77,11 +92,15 @@ def mark_item_complete(request, item_id):
     if request.method == 'PUT':
         if not item_id:
             return JsonResponse({'error': 'Item Not Found'}, status=404)
-        item = get_object_or_404(Item, id=item_id, list__user=request.user)
-        item.completed = True
-        item.save()
-        return JsonResponse({'message': 'Item Marked as Completed'}, status=202)
+        try:
+            item = get_object_or_404(Item, id=item_id, list__user=request.user)
+            item.completed = True
+            item.save()
+            return JsonResponse({'message': 'Item Marked as Completed'}, status=202)
+        except Exception as e:
+            return JsonResponse({'error': 'Unable to mark as complete, Try again later'}, status=404)
     return JsonResponse({"error": "Invalid request method."}, status=400)
+
 
 # Delete List
 @login_required
@@ -89,17 +108,24 @@ def delete_list(request, list_id):
     if request.method == 'DELETE':
         if not list_id:
             return JsonResponse({'error': 'List Not Found'}, status=404)
-        list_obj = get_object_or_404(List, id=list_id, user=request.user)
-        list_obj.delete()
-        return JsonResponse({'message': 'List Deleted'}, status=202)
+        try:
+            list_obj = get_object_or_404(List, id=list_id, user=request.user)
+            list_obj.delete()
+            return JsonResponse({'message': 'List Deleted'}, status=202)
+        except Exception as e:
+            return JsonResponse({'error': 'Unable to delete list, Try again later'}, status=404)
     return JsonResponse({"error": "Invalid request method."}, status=400)
+
 
 @login_required
 def delete_item(request, item_id):
     if request.method == 'DELETE':
         if not item_id:
             return JsonResponse({'error': 'Item Not Found'}, status=404)
-        item = get_object_or_404(Item, id=item_id)
-        item.delete()
-        return JsonResponse({"message": "Item deleted successfully."})
+        try:
+            item = get_object_or_404(Item, id=item_id)
+            item.delete()
+            return JsonResponse({"message": "Item deleted successfully."})
+        except Exception as e:
+            return JsonResponse({'error': 'Unable to delete item, Try again later'}, status=404)
     return JsonResponse({"error": "Invalid request method."}, status=400)
